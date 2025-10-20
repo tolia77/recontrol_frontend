c21693997df609308680491f96ba219bf396965c
tolia k <tolia.kohan@gmail.com>
2025-10-20 18:18:32 +0300

    create project structure

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from "@tailwindcss/vite";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+    resolve: {
+        alias: {
+            'src': path.resolve('src'),
+        }
+    },
+    plugins: [
+        react(),
+        tailwindcss()
+    ],
+    server: {
+        port: 5175,
+    }
 })
