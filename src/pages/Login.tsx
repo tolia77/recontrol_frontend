cba5e1ea7df86d4344753377eb85ae4aca41526c
tolia k <tolia.kohan@gmail.com>
2025-10-21 22:01:41 +0300

    finish styling login page

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -9,19 +9,19 @@ function Login() {
     }
     return (
         <div className="min-h-screen flex items-center justify-center">
-            <div className="flex flex-col items-center space-y-4">
+            <div className="flex flex-col items-center space-y-8">
                 <img src={logoFull} alt="logo" />
                 <h1>Log in your account</h1>
-                <form onSubmit={handleSubmit} className="space-y-4">
+                <form onSubmit={handleSubmit} className="space-y-6 min-w-[350px]">
                     <div>
                         <label className="text-body-small" htmlFor="email">Email:</label>
                         <br/>
-                        <input type="email" id="email" name="email" required />
+                        <input className="w-full" type="email" id="email" name="email" required />
                     </div>
                     <div>
                         <label className="text-body-small" htmlFor="password">Password:</label>
                         <br/>
-                        <input type="password" id="password" name="password" required />
+                        <input className="w-full" type="password" id="password" name="password" required />
                     </div>
                     <button className="button-primary w-full" type="submit">Log In</button>
                     <p>Don't have an account? <Link className="text-secondary" to={"/signup"}>Sign up</Link></p>
