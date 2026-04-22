15d17b8950a52ba6d32f8121355907031854b8c0
tolia k <tolia.kohan@gmail.com>
2026-04-22 15:03:57 +0300

    move toast notifications to top right

--- src/components/ui/Toast.tsx
+++ src/components/ui/Toast.tsx
@@ -87,7 +87,7 @@ function ToastContainer({ toasts, removeToast }: ToastContainerProps) {
   if (toasts.length === 0) return null;
 
   return (
-    <div className="fixed bottom-4 right-4 z-[9999] flex flex-col gap-2 max-w-sm">
+    <div className="fixed top-4 right-4 z-[9999] flex flex-col gap-2 max-w-sm">
       {toasts.map((toast) => (
         <div
           key={toast.id}
