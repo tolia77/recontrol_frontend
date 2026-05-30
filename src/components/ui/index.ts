168a73b446f8ca81da3a12e2df686e0ab7abf457
tolia k <tolia.kohan@gmail.com>
2026-05-30 12:04:41 +0300

    add reusable banner ui primitive

--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -1,3 +1,4 @@
+export { default as Banner } from "./Banner";
 export { default as Button } from "./Button";
 export { default as Spinner } from "./Spinner";
 export { default as LoadingOverlay } from "./LoadingOverlay";
